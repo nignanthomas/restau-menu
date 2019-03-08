eb8588cb5bb0a8661fd752c1a6014952f6c5734c
nignanthomas <nignanthomas@gmail.com>
2019-03-08 16:07:03 +0300

    tuto completed

---
+++ src/components/counters.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  
+
+  render() {
+
+    console.log('Counters - Rendered');
+    
+    const {onReset, counters, onDelete, onIncrement} = this.props
+
+    return (
+      <div>
+        <button
+          onClick={onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            // value={counter.value}
+            // id={counter.id}
+            counter={counter} // we can pass the object itself instead of listing all of its properties
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
